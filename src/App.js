2c991a7136c96ed262a93ef678a02b685300c455
chrissundal <christoffersj@hotmail.com>
2025-03-20 08:42:21 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<h1>Hello World</h1>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
